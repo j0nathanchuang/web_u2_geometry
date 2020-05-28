38c94906a83fef21fb3cb44a0e949cea2d74e638
Jonathan Chuang <jochuang@umich.edu>
2020-05-28 13:47:41 -0400

    Added function declarations

---
+++ starter_code/main.js
@@ -0,0 +1,38 @@
+/*
+ * HARDEST FUNCTION
+ */
+function fixHeadings() {
+
+}
+
+function square() {
+
+}
+
+function rectangle() {
+
+}
+
+function circle() {
+
+}
+
+function triangle() {
+
+}
+
+function cube() {
+
+}
+
+function cylinder() {
+
+}
+
+function sphere() {
+
+}
+
+function cone() {
+
+}
