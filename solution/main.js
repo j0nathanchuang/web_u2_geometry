1b51d875c8f4c113bc919c6503f38a571a628cbd
Jonathan Chuang <jochuang@umich.edu>
2020-05-28 21:28:18 -0400

    Initial commit

---
+++ solution/main.js
@@ -0,0 +1,82 @@
+/*
+ * HARDEST FUNCTION
+ * Purpose: The headings got all mixed up, it would be easier to just write
+ *          a function to fix this, instead of changing the HTML code. (Not
+ *          really but for the purpose of learning, run with it)
+ */
+function fixHeadings() {
+  //HINT: Use the API that can get all of the name tags
+
+}
+
+/*
+ * Purpose: Since we have yet to reach JavaScript's built-in functions, use this
+ *          "self-written" power function
+ */
+function power(x, y) {
+  return Math.pow(x, y);
+}
+
+/*
+ * Purpose: Prompt the user for the side length, calculate the area, and
+ *          change the text from "The area is: " to "The area is: [area]"
+ */
+function square() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the both side lengths, calculate the area, and
+ *          change the text from "The area is: " to "The area is: [area]"
+ */
+function rectangle() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the radius, calculate the area, and
+ *          change the text from "The area is: " to "The area is: [area]"
+ */
+function circle() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the base & height, calculate the area,
+ *          and change the text from "The area is: " to "The area is: [area]"
+ */
+function triangle() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the side length, calculate the volume, and
+ *          change the text from "The volume is: " to "The volume is: [volume]"
+ */
+function cube() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the radius & height, calculate the volume, and
+ *          change the text from "The volume is: " to "The volume is: [volume]"
+ */
+function cylinder() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the radius, calculate the volume, and
+ *          change the text from "The volume is: " to "The volume is: [volume]"
+ */
+function sphere() {
+
+}
+
+/*
+ * Purpose: Prompt the user for the radius & height, calculate the volume, and
+ *          change the text from "The volume is: " to "The volume is: [volume]"
+ */
+function cone() {
+
+}
